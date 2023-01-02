fed303c6e212f800f3d51220732e543fb59c1be5
jbonaiuto <jbonaiuto@gmail.com>
2023-01-02 10:28:24 +0100

    refactoring

--- align_videos.py
+++ align_videos.py
@@ -1,5 +1,5 @@
+import glob
 import os
-import sys
 import wave
 
 import cv2
@@ -22,113 +22,122 @@ def quick_resize(data, scale, og_width, og_height):
     return resized
 
 
-# Videos to align
-vid_1 = sys.argv[1]
-vid_2 = sys.argv[2]
+def align_behav_videos(behav_path, behav_deriv_path):
+    n_aligned=0
+    videos=sorted(glob.glob(os.path.join(behav_path,'*.MP4')))
+    cam_years=sorted(list(set([os.path.split(x)[1].split('_')[0] for x in videos])))
+    cam_1_videos = [s for s in videos if 'behav/{}_'.format(cam_years[0]) in s]
+    cam_2_videos = [s for s in videos if 'behav/{}_'.format(cam_years[1]) in s]
+    for vid_idx,cam_1_vid in enumerate(cam_1_videos):
+        cam_2_vid=cam_2_videos[vid_idx]
+        align_two_videos(cam_1_vid, cam_2_vid, behav_deriv_path)
+        n_aligned+=1
+    return n_aligned
 
-# Align videos
-file_specs = check_and_decode_filenames([vid_1, vid_2], min_num_files=2)
-with SyncDetector() as det:
-    result = det.align(file_specs)
+def align_two_videos(vid_1, vid_2, output_path):
+    vid_1_name = os.path.splitext(os.path.split(vid_1)[1])[0]
+    vid_2_name = os.path.splitext(os.path.split(vid_2)[1])[0]
+    combined_video_fname = os.path.join(output_path, "combined_{}_{}.avi".format(vid_1_name, vid_2_name))
 
-# Amount to trim from beginning of video 1 (seconds)
-vid1_trim=result[0]['trim']
-vid2_trim=result[1]['trim']
+    if not os.path.exists(combined_video_fname):
+        # Align videos
+        file_specs = check_and_decode_filenames([vid_1, vid_2], min_num_files=2)
+        with SyncDetector() as det:
+            result = det.align(file_specs)
 
-# FPS
-fps = result[0]['orig_streams'][0]['fps']
-# Number of frames to trim from beginning of video 1
-if np.abs(vid1_trim)>np.abs(vid2_trim):
-    offset = int(vid1_trim * fps)
-else:
-    offset=-int(vid2_trim*fps)
+        # Amount to trim from beginning of video 1 (seconds)
+        vid1_trim=result[0]['trim']
+        vid2_trim=result[1]['trim']
 
-# Open videos
-vidcap1 = cv2.VideoCapture(vid_1)
-vidcap2 = cv2.VideoCapture(vid_2)
+        # FPS
+        fps = result[0]['orig_streams'][0]['fps']
+        # Number of frames to trim from beginning of video 1
+        if np.abs(vid1_trim)>np.abs(vid2_trim):
+            offset = int(vid1_trim * fps)
+        else:
+            offset=-int(vid2_trim*fps)
 
-# Get output path and filenames without extensions
-output_path = os.path.split(vid_1)[0]
-vid_1_name = os.path.splitext(os.path.split(vid_1)[1])[0]
-vid_2_name = os.path.splitext(os.path.split(vid_2)[1])[0]
+        # Open videos
+        vidcap1 = cv2.VideoCapture(vid_1)
+        vidcap2 = cv2.VideoCapture(vid_2)
 
-# Convert vid1 audio to wav
-audio_file = '{}.wav'.format(os.path.splitext(vid_1)[0])
-cmd = ['ffmpeg', '-i', vid_1, audio_file]
-subprocess.run(cmd)
 
-# Read audio data
-audio = wave.open(audio_file, mode='rb')
-audio_params = audio.getparams()
-audio_data = audio.readframes(audio_params.nframes)
-audio_data = np.frombuffer(audio_data, dtype=np.int16)  # Audio format is usually 16 bits; buffer
+        # Convert vid1 audio to wav
+        audio_file = os.path.join(output_path,'{}.wav'.format(os.path.splitext(vid_1)[0]))
+        cmd = ['ffmpeg', '-i', vid_1, audio_file]
+        subprocess.run(cmd)
 
-# Video 1 starts too soon
-if offset > 0:
-    for i in range(offset):
-        vidcap1.read()
-    audio_offset = vid1_trim
-# Video 2 starts too soon
-else:
-    for i in range(-offset):
-        vidcap2.read()
-    audio_offset = 0
+        # Read audio data
+        audio = wave.open(audio_file, mode='rb')
+        audio_params = audio.getparams()
+        audio_data = audio.readframes(audio_params.nframes)
+        audio_data = np.frombuffer(audio_data, dtype=np.int16)  # Audio format is usually 16 bits; buffer
 
-# Crop audio
-audio_part = audio_data[int(audio_offset * audio_params.framerate):]
+        # Video 1 starts too soon
+        if offset > 0:
+            for i in range(offset):
+                vidcap1.read()
+            audio_offset = vid1_trim
+        # Video 2 starts too soon
+        else:
+            for i in range(-offset):
+                vidcap2.read()
+            audio_offset = 0
 
-# Video size
-f_size = (1920, 540)
+        # Crop audio
+        audio_part = audio_data[int(audio_offset * audio_params.framerate):]
 
-# Write aligned video without audio
-fourcc = cv2.VideoWriter_fourcc(*'MJPG')
-silent_video_fname = os.path.join(output_path, "combined_video_silent.avi")
-cam_vid = cv2.VideoWriter(
-    silent_video_fname,
-    fourcc,
-    float(fps),
-    f_size
-)
+        # Video size
+        f_size = (1920, 540)
 
-# For each frame
-while True:
-    # Read frames
-    success1, image1 = vidcap1.read()
-    success2, image2 = vidcap2.read()
+        # Write aligned video without audio
+        fourcc = cv2.VideoWriter_fourcc(*'MJPG')
+        silent_video_fname = os.path.join(output_path, "combined_video_silent.avi")
+        cam_vid = cv2.VideoWriter(
+            silent_video_fname,
+            fourcc,
+            float(fps),
+            f_size
+        )
 
-    # Break if at least one video has ended
-    if not success1 and success2:
-        image1=np.ones(image2.shape).astype(np.uint8)
-    elif not success2 and success1:
-        image2 = np.ones(image1.shape).astype(np.uint8)
-    elif not (success1 or success2):
-        break
+        # For each frame
+        while True:
+            # Read frames
+            success1, image1 = vidcap1.read()
+            success2, image2 = vidcap2.read()
 
-    # Resize each frame
-    resized1 = quick_resize(image1, 0.5, image1.shape[1], image1.shape[0])
-    resized2 = quick_resize(image2, 0.5, image2.shape[1], image2.shape[0])
+            # Break if at least one video has ended
+            if not success1 and success2:
+                image1=np.ones(image2.shape).astype(np.uint8)
+            elif not success2 and success1:
+                image2 = np.ones(image1.shape).astype(np.uint8)
+            elif not (success1 or success2):
+                break
 
-    # Show camera images side by side
-    data = np.hstack([resized1, resized2])
-    cam_vid.write(data[:, :, :3])
+            # Resize each frame
+            resized1 = quick_resize(image1, 0.5, image1.shape[1], image1.shape[0])
+            resized2 = quick_resize(image2, 0.5, image2.shape[1], image2.shape[0])
 
-cam_vid.release()
+            # Show camera images side by side
+            data = np.hstack([resized1, resized2])
+            cam_vid.write(data[:, :, :3])
 
-# Write cropped audio to file
-fp = wave.Wave_write(audio_file)
-# Set the parameters of fp
-fp.setframerate(audio_params.framerate)
-fp.setnframes(len(audio_part))
-fp.setnchannels(audio_params.nchannels)
-fp.setsampwidth(audio_params.sampwidth)
-fp.writeframes(audio_part.tobytes())
-fp.close()
+        cam_vid.release()
 
-# Combine video and audio
-combined_video_fname = os.path.join(output_path, "combined_{}_{}.avi".format(vid_1_name, vid_2_name))
-cmd = ['ffmpeg', '-i', audio_file, '-i', silent_video_fname, '-q:v', '0', combined_video_fname]
-subprocess.run(cmd)
+        # Write cropped audio to file
+        fp = wave.Wave_write(audio_file)
+        # Set the parameters of fp
+        fp.setframerate(audio_params.framerate)
+        fp.setnframes(len(audio_part))
+        fp.setnchannels(audio_params.nchannels)
+        fp.setsampwidth(audio_params.sampwidth)
+        fp.writeframes(audio_part.tobytes())
+        fp.close()
 
-# Remove temporary files
-os.remove(audio_file)
-os.remove(silent_video_fname)
+        # Combine video and audio
+        cmd = ['ffmpeg', '-i', audio_file, '-i', silent_video_fname, '-q:v', '0', combined_video_fname]
+        subprocess.run(cmd)
+
+        # Remove temporary files
+        os.remove(audio_file)
+        os.remove(silent_video_fname)
